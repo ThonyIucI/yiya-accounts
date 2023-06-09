bbb40f003ff5ed1c36c953a8807998b932e2ad41
thony <desarrollo2.punku@gmail.com>
2023-06-09 16:35:38 -0500

    modulo profile actualizado

--- src/modules/profiles/profiles.service.ts
+++ src/modules/profiles/profiles.service.ts
@@ -1,26 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Profile } from './entities/profile.entity';
+import { Repository } from 'typeorm';
+import {
+  createdMessage,
+  deletedMessage,
+  updatedMessage,
+} from 'src/utils/message.manager';
+import { ErrorManager } from 'src/utils/error.manager';
 
 @Injectable()
 export class ProfilesService {
-  create(createProfileDto: CreateProfileDto) {
-    return 'This action adds a new profile';
+  constructor(
+    @InjectRepository(Profile)
+    private readonly profileRepository: Repository<Profile>,
+  ) {}
+  async verifyDuplicated(profileName: string, id?: number) {
+    const existingSupplier = await this.profileRepository.findOne({
+      where: { profileName },
+    });
+
+    if (existingSupplier && existingSupplier.id !== id) {
+      throw new ConflictException(
+        'El nombre del proveedor ya está en uso. Por favor, ingrese un nombre único.',
+      );
+    }
+  }
+  async create(createProfileDto: CreateProfileDto) {
+    try {
+      const item = await this.profileRepository.save(createProfileDto);
+      if (item) return createdMessage(item);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all profiles`;
+  async findAll() {
+    try {
+      return await this.profileRepository.find();
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} profile`;
+  async findOne(id: number) {
+    try {
+      return await this.profileRepository.findOneOrFail({ where: { id } });
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error);
+    }
   }
 
-  update(id: number, updateProfileDto: UpdateProfileDto) {
-    return `This action updates a #${id} profile`;
+  async update(id: number, updateProfileDto: UpdateProfileDto) {
+    try {
+      await this.profileRepository.update(id, updateProfileDto);
+      const item = await this.findOne(id);
+      return updatedMessage(item);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} profile`;
+  async remove(id: number) {
+    try {
+      await this.profileRepository.delete(id);
+      return deletedMessage();
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error);
+    }
   }
 }
